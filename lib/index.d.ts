a572c6c43f348e376951d48b657a1e0eb4001bfd
megues <marcus@felfel.ch>
2019-02-01 15:58:22 +0100

    Remove lib and .idea files

--- lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { LoggingStore, IExceptionInfo, ILogMessage, ILogger, Logger } from './Logger';
-export { BatchedSinkOptions, HttpSinkOptions, ILogSink, BatchedSink, HttpSink } from './Sinks';
