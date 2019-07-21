0a5f227103d7eec3878ade820acb1d1b47958665
hardcodet <philipp@hardcodet.net>
2019-07-21 14:38:10 +0200

    Including exceptions and colored output.

--- lib/Sinks/ConsoleSink.d.ts
+++ lib/Sinks/ConsoleSink.d.ts
@@ -1,6 +1,10 @@
 import { ILogSink } from "./ILogSink";
 import { ILogMessage } from "../Logger/ILogMessage";
 export declare class ConsoleSink implements ILogSink {
+    RESET: string;
+    INFO: string;
+    WARN: string;
+    ERROR: string;
     flush(): Promise<void>;
     log(message: ILogMessage): void;
 }
--- src/Sinks/ConsoleSink.ts
+++ src/Sinks/ConsoleSink.ts
@@ -3,12 +3,28 @@ import {ILogMessage} from "../Logger/ILogMessage";
 import {LogLevel} from "../Logger/LogLevel";
 
 export class ConsoleSink implements ILogSink {
+
+    RESET = "\x1b[0m\]";
+    INFO = "\x1b[34m\]";
+    WARN = "\x1b[33m\]";
+    ERROR = "\x1b[31m\]";
+
+
     public async flush(): Promise<void> {
         return;
     }
 
     public log(message: ILogMessage) {
         let msg: string = `${message.level.toUpperCase()}: ${message.context} --> ${message.message}`;
+
+        // include stack trace
+        if (message.exception) {
+            msg = `${msg}
+${message.exception.errorMessage}
+${message.exception.stackTrace}`
+        }
+
+        // include payload
         if (message.payload) {
             msg = `${msg}
 ${JSON.stringify(message.payload)}`;
@@ -16,16 +32,18 @@ ${JSON.stringify(message.payload)}`;
 
         switch (message.level) {
             case LogLevel.Debug:
-            case LogLevel.Info:
                 console.log(msg);
                 break;
+            case LogLevel.Info:
+                console.log(this.INFO + msg, this.RESET);
+                break;
             case LogLevel.Warning:
-                console.warn(msg);
+                console.warn(this.WARN + msg, this.RESET);
                 break;
             case LogLevel.Error:
             case LogLevel.Fatal:
             default:
-                console.error(msg);
+                console.error(this.ERROR + msg, this.RESET);
                 break;
         }
     }
